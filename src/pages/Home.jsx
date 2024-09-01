eba76c927b192a990b65d0aa5efa9d80257a30d5
Aku Sarma <akusarma1702@gmail.com>
2024-09-01 21:10:06 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <h1>Home</h1>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Profile = () => {
+  return (
+    <div>
+        <Navbar/>
+        <h1>Profile</h1>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Profile
