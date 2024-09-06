101ecdeacf2db94e4d284e1a8facc952e586da36
Aku Sarma <akusarma1702@gmail.com>
2024-09-06 20:38:07 +0530

    setup of backend files

---
+++ jiLancerFrontend/src/pages/Portfolio.jsx
@@ -0,0 +1,28 @@
+/*
+Freelancer
+================
+Name
+rating
+Bio
+social media links
+skill
+projects
+completed work
+
+Employer
+================
+Name
+rating
+bio
+social media links
+projects given
+
+*/
+
+const Portfolio = () => {
+  return (
+    <div>Portfolio</div>
+  )
+}
+
+export default Portfolio
