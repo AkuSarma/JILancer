53c96e947f2d5ee33d8a0b994aa537eb8efe16ea
souvik debbarma <souvikdbarma23@gmail.com>
2024-09-09 21:24:59 +0530

    top container fixed

--- jiLancerFrontend/src/pages/Home.jsx
+++ jiLancerFrontend/src/pages/Home.jsx
@@ -1,48 +1,104 @@
-import BelowHomePage from '../pages/BelowHomePage'
+import BelowHomePage from '../pages/BelowHomePage';
 
 const Home = () => {
   return (
-    <div className=''>
-      <div className="grid grid-cols-2 gap-2 space-x-0 justify my-5 ">
-        {/* left card */}
-        <div className="h-auto w-auto border- rounded-[1.5em] bg-primaryColor to-lime200 text-black font-nunito p-[1em] flex justify-center items-left flex-col gap-[0.75em] backdrop-blur-[12px] ">
-          <div className="px-2 py-2">
-            <h1 className="text-[2em] font-medium">Get Hired for Your Next Gig </h1>
-            <p className="text-[0.85em]">
-              Connecting Skilled Freelancers with Companies Ready to Innovate
-            </p>
+    <div className=" ">
+      <div className="flex justify-between space-x-5 my-5">
+        {/* Left Div */}
+        <div className="flex flex-col w-[65%] space-y-5">
+          {/* Left Main Card */} 
+          <div className="h-auto w-auto border- rounded-[1.5em] bg-primaryColor text-black font-nunito p-[1em] flex justify-center items-left flex-col gap-[0.75em] backdrop-blur-[12px]">
+            <div className="px-2 py-2">
+              <h1 className="text-[2em] font-medium">Get Hired for Your Next Gig</h1>
+              <p className="text-[0.85em]">
+                Connecting Skilled Freelancers with Companies Ready to Innovate
+              </p>
+            </div>
+            <button className="h-fit w-fit px-[1em] py-[0.25em] border-[1px] rounded-full flex justify-center items-center gap-[1em] overflow-hidden group hover:translate-y-[0.125em] duration-200 backdrop-blur-[12px]">
+              <p>Button</p>
+              <svg
+                className="w-6 h-6 group-hover:translate-x-[10%] duration-300"
+                stroke="currentColor"
+                strokeWidth="1"
+                viewBox="0 0 24 24"
+                fill="white"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                ></path>
+              </svg>
+            </button>
           </div>
-          <button className=" h-fit w-fit px-[1em] py-[0.25em] border-[1px] rounded-full flex justify-center items-center gap-[1em] overflow-hidden group hover:translate-y-[0.125em] duration-200 backdrop-blur-[12px]">
-            <p>Button</p>
-            <svg
-              className="w-6 h-6 group-hover:translate-x-[10%] duration-300"
-              stroke="currentColor"
-              strokeWidth="1"
-              viewBox="0 0 24 24"
-              fill="white"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-                strokeLinejoin="round"
-                strokeLinecap="round"
-              ></path>
-            </svg>
-          </button>
+
+          {/* Below Cards (Merged with Left) */}
+          <div className="flex space-x-5">
+            <div className="h-[16em] w-[18em] border- rounded-[1.5em] bg-primaryColor text-black font-nunito p-[1em] flex justify-center items-left flex-col gap-[0.75em] backdrop-blur-[12px]">
+              <div className="px-2 py-2">
+                <h1 className="text-[2em] font-medium">Heading 1</h1>
+                <p className="text-[0.85em]">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </div>
+              <button className="h-fit w-fit px-[1em] py-[0.25em] border-[1px] rounded-full flex justify-center items-center gap-[1em] overflow-hidden group hover:translate-y-[0.125em] duration-200 backdrop-blur-[12px]">
+                <p>Button</p>
+                <svg
+                  className="w-6 h-6 group-hover:translate-x-[10%] duration-300"
+                  stroke="currentColor"
+                  strokeWidth="1"
+                  viewBox="0 0 24 24"
+                  fill="white"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+
+            <div className="h-[16em] w-[18em] border- rounded-[1.5em] bg-primaryColor text-black font-nunito p-[1em] flex justify-center items-left flex-col gap-[0.75em] backdrop-blur-[12px]">
+              <div className="px-2 py-2">
+                <h1 className="text-[2em] font-medium">Heading 2</h1>
+                <p className="text-[0.85em]">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </div>
+              <button className="h-fit w-fit px-[1em] py-[0.25em] border-[1px] rounded-full flex justify-center items-center gap-[1em] overflow-hidden group hover:translate-y-[0.125em] duration-200 backdrop-blur-[12px]">
+                <p>Button</p>
+                <svg
+                  className="w-6 h-6 group-hover:translate-x-[10%] duration-300"
+                  stroke="currentColor"
+                  strokeWidth="1"
+                  viewBox="0 0 24 24"
+                  fill="white"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+          </div>
         </div>
 
-        {/* right card */}
-
-        <div className="w-800 h-[40rem]  rounded-[30px] bg-lastColor flex flex-col justify-between">
+        {/* Right Div */}
+        <div className="w-[35%] h-[40rem] rounded-[30px] bg-lastColor flex flex-col justify-between">
           <div className="px-5 py-5">
             <h1 className="text-[2em] font-medium">Heading</h1>
             <p className="text-[0.85em]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-              dolorum blanditiis pariatur sequi magni.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dolorum blanditiis pariatur sequi magni.
             </p>
           </div>
           <div className="flex items-center space-x-6 justify-center">
-            <a className="  mb-5 h-fit w-fit px-[1em] py-[0.25em] border-[1px] rounded-full flex justify-center items-center gap-[0.5em] overflow-hidden group hover:translate-y-[0.125em] duration-200 backdrop-blur-[12px]">
+            <a className="mb-5 h-fit w-fit px-[1em] py-[0.25em] border-[1px] rounded-full flex justify-center items-center gap-[0.5em] overflow-hidden group hover:translate-y-[0.125em] duration-200 backdrop-blur-[12px]">
               <p>Button</p>
               <svg
                 className="w-6 h-6 group-hover:translate-x-[10%] duration-300"
@@ -60,7 +116,7 @@ const Home = () => {
               </svg>
             </a>
 
-            <a className="  mb-5 h-fit w-fit px-[1em] py-[0.25em] border-[1px] rounded-full flex justify-center items-center gap-[0.5em] overflow-hidden group hover:translate-y-[0.125em] duration-200 backdrop-blur-[12px]">
+            <a className="mb-5 h-fit w-fit px-[1em] py-[0.25em] border-[1px] rounded-full flex justify-center items-center gap-[0.5em] overflow-hidden group hover:translate-y-[0.125em] duration-200 backdrop-blur-[12px]">
               <p>Button</p>
               <svg
                 className="w-6 h-6 group-hover:translate-x-[10%] duration-300"
@@ -77,66 +133,11 @@ const Home = () => {
                 ></path>
               </svg>
             </a>
-          </div>
-        </div>
-
-        {/* below card */}
-        <div className="flex items-center space-x-6 justify-start mb-5">
-          <div className="h-[16em] w-[18em]  border- rounded-[1.5em] bg-primaryColor  text-black font-nunito p-[1em] flex justify-center items-left flex-col gap-[0.75em] backdrop-blur-[12px] ">
-            <div className="px-2 py-2">
-              <h1 className="text-[2em] font-medium">Heading</h1>
-              <p className="text-[0.85em]">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-                dolorum blanditiis pariatur sequi magni.
-              </p>
-            </div>
-            <button className=" h-fit w-fit px-[1em] py-[0.25em] border-[1px] rounded-full flex justify-center items-center gap-[1em] overflow-hidden group hover:translate-y-[0.125em] duration-200 backdrop-blur-[12px]">
-              <p>Button</p>
-              <svg
-                className="w-6 h-6 group-hover:translate-x-[10%] duration-300"
-                stroke="currentColor"
-                strokeWidth="1"
-                viewBox="0 0 24 24"
-                fill="white"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                ></path>
-              </svg>
-            </button>
-          </div>
-
-          <div className="h-[16em] w-[18em] border- rounded-[1.5em]  bg-primaryColor text-black font-nunito p-[1em] flex justify-center items-left flex-col gap-[0.75em] backdrop-blur-[12px] ">
-            <div className="px-2 py-2">
-              <h1 className="text-[2em] font-medium">Heading</h1>
-              <p className="text-[0.85em]">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-                dolorum blanditiis pariatur sequi magni.
-              </p>
-            </div>
-            <button className=" h-fit w-fit px-[1em] py-[0.25em] border-[1px] rounded-full flex justify-center items-center gap-[1em] overflow-hidden group hover:translate-y-[0.125em] duration-200 backdrop-blur-[12px]">
-              <p>Button</p>
-              <svg
-                className="w-6 h-6 group-hover:translate-x-[10%] duration-300"
-                stroke="currentColor"
-                strokeWidth="1"
-                viewBox="0 0 24 24"
-                fill="white"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                ></path>
-              </svg>
-            </button>
           </div>
         </div>
       </div>
+
+      {/* BelowHomePage Component */}
       <BelowHomePage />
     </div>
   );
