8de7d985fcbf23cf386f3c076fd917b0e9789f5f
Aku Sarma <akusarma1702@gmail.com>
2024-09-09 13:38:44 +0530

    implemented loading in login and signup

--- jiLancerFrontend/src/components/SignUp.jsx
+++ jiLancerFrontend/src/components/SignUp.jsx
@@ -1,51 +1,58 @@
-import PropTypes from 'prop-types';
-import { useState } from 'react';
+import PropTypes from "prop-types";
+import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import RoleIdentifier from './RoleIdentifier';
+import RoleIdentifier from "./RoleIdentifier";
+import { RingLoader } from "react-spinners"; // Import the spinner
 
 const SignUp = ({ toggleForms }) => {
   const navigate = useNavigate();
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [details, setDetails] = useState(false)
+  const [details, setDetails] = useState(false);
+  const [loading, setLoading] = useState(false); // State to manage loading
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
 
-   const [username, setUsername] = useState("");
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
-   const [isFreelancer, setIsFreelancer] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-   const handleSubmit = async (e) => {
-    console.log(isFreelancer)
-     e.preventDefault();
-     try {
-       const response = await axios.post(
-         "http://localhost:8000/user_authentication/register/",
-         {
-           username,
-           email,
-           password,
-           is_freelancer: isFreelancer,
-         }
-       );
-       setDetails(false);
-       navigate("/Auth?=false");
-       console.log("Registration successful:", response.data);
-     } catch (error) {
-       console.error("Error during registration:", error);
-     }
-   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true); // Start loading
+    console.log("trying to submit")
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/user_authentication/register/",
+        {
+          username,
+          email,
+          password,
+          is_freelancer: isFreelancer,
+        }
+      );
+      setDetails(false);
+      setLoading(true);
+      console.log("Registration successful:", response.data);
+      navigate("/Auth");
+    } catch (error) {
+      console.error("Error during registration:", error);
+    } finally {
+      setLoading(false); // Stop loading when done
+    }
+  };
 
-  if (details == true){
-    return(
+  if (details) {
+    return (
       <RoleIdentifier
-        handleSubmit={handleSubmit}
-        setIsFreelancer={setIsFreelancer}
+        username={username}
+        email={email}
+        password={password}
+        setDetails={setDetails}
       />
-    )
+    );
   } else {
     return (
       <div className="h-full flex justify-center items-center bg-lastColor rounded-lg py-10 px-8 my-4">
@@ -53,65 +60,59 @@ const SignUp = ({ toggleForms }) => {
         <div className="w-1/2 p-10 flex flex-col justify-center bg-white text-primaryColor rounded-lg mx-2">
           <h2 className="text-3xl font-bold text-green-800">Sign up</h2>
           <p className="text-green-600 mb-6">
-            Welcome back! Please login to your account
+            Welcome back! Please register for a new account
           </p>
-          <form className="space-y-4" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="Username"
-              className="w-full p-3 rounded-full bg-primaryColor focus:outline-primaryColor focus:bg-white text-white focus:text-lastColor"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              className="w-full p-3 rounded-full bg-primaryColor focus:outline-primaryColor focus:bg-white text-white focus:text-lastColor"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type={passwordVisible ? "text" : "password"}
-              placeholder="Password"
-              className="w-full p-3 rounded-full bg-primaryColor focus:outline-primaryColor focus:bg-white text-white focus:text-lastColor"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <input
-              type={passwordVisible ? "text" : "password"}
-              placeholder="Confirm password"
-              className="w-full p-3 rounded-full bg-primaryColor focus:outline-primaryColor focus:bg-white text-white focus:text-lastColor"
-            />
-            {/* Show password checkbox */}
-            <div className="flex items-center mb-6">
-              <input
-                type="checkbox"
-                id="showPassword"
-                className="mr-2"
-                checked={passwordVisible}
-                onChange={togglePasswordVisibility}
-              />
-              <label htmlFor="showPassword" className="text-gray-700">
-                Show Password
-              </label>
+          {loading ? (
+            <div className="flex justify-center items-center h-full">
+              <RingLoader size={60} color={"#3498db"} loading={loading} />
             </div>
-            {/* <div className="mb-4">
-            <label htmlFor="isFreelancer" className="block text-gray-700 mb-2">
-              Are you a freelancer?
-            </label>
-            <input
-              id="isFreelancer"
-              className="w-full px-4 py-2 rounded-full bg-backgroundColor text-gray-700 focus:transform focus:scale-105 transition-transform duration-200 ease-in-out"
-              type="checkbox"
-              checked={isFreelancer}
-              onChange={() => setIsFreelancer(!isFreelancer)}
-            />
-          </div> */}
-            <button
-              type=""
-              className="mt-4 bg-primaryColor hover:bg-green-500 text-white p-3 rounded-full w-full"
-              onClick={() => setDetails(true)}
-            >
-              Next
-            </button>
-          </form>
+          ) : (
+            <form className="space-y-4" onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Username"
+                className="w-full p-3 rounded-full bg-primaryColor focus:outline-primaryColor focus:bg-white text-white focus:text-lastColor"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                className="w-full p-3 rounded-full bg-primaryColor focus:outline-primaryColor focus:bg-white text-white focus:text-lastColor"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type={passwordVisible ? "text" : "password"}
+                placeholder="Password"
+                className="w-full p-3 rounded-full bg-primaryColor focus:outline-primaryColor focus:bg-white text-white focus:text-lastColor"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input
+                type={passwordVisible ? "text" : "password"}
+                placeholder="Confirm password"
+                className="w-full p-3 rounded-full bg-primaryColor focus:outline-primaryColor focus:bg-white text-white focus:text-lastColor"
+              />
+              {/* Show password checkbox */}
+              <div className="flex items-center mb-6">
+                <input
+                  type="checkbox"
+                  id="showPassword"
+                  className="mr-2"
+                  checked={passwordVisible}
+                  onChange={togglePasswordVisibility}
+                />
+                <label htmlFor="showPassword" className="text-gray-700">
+                  Show Password
+                </label>
+              </div>
+              <button
+                type="button"
+                className="mt-4 bg-primaryColor hover:bg-green-500 text-white p-3 rounded-full w-full"
+                onClick={() => setDetails(true)}
+              >
+                Next
+              </button>
+            </form>
+          )}
         </div>
         {/* Log in section */}
         <div className="w-1/2 h-full bg-primaryColor rounded-r-lg flex flex-col justify-center items-center rounded-lg mx-2">
@@ -126,7 +127,7 @@ const SignUp = ({ toggleForms }) => {
 };
 
 SignUp.propTypes = {
-  toggleForms: PropTypes.func.isRequired,  // Define the expected type and that it's required
+  toggleForms: PropTypes.func.isRequired, // Define the expected type and that it's required
 };
 
 export default SignUp;
