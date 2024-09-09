9ad8c33d5f2a825a4f3eca63cb9bc2ce4096c883
Aku Sarma <akusarma1702@gmail.com>
2024-09-09 14:37:18 +0530

    changed company name

--- jiLancerFrontend/src/components/Footer.jsx
+++ jiLancerFrontend/src/components/Footer.jsx
@@ -3,11 +3,10 @@ const Footer = () => {
   return (
     <footer className="bg-primaryColor text-white py-4 text-center">
       <div className="container mx-auto px-4">
-        <h2 className="text-xl font-bold mb-2">Udte Punjab Karega Kaam</h2>
+        <h2 className="text-xl font-bold mb-2">JIlancer</h2>
         <p className="">Innovative Solutions for a Brighter Future</p>
         <p className=" mt-4">
-          © {new Date().getFullYear()} Udte Punjab Karega Kaam. All rights
-          reserved.
+          © {new Date().getFullYear()} JIlancer. All rights reserved.
         </p>
       </div>
     </footer>
